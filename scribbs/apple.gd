365ea9af411d9bfe92c21128c0defb9ca62cfdb9
Omelette <desh.kian@gmail.com>
2026-06-09 20:30:17 -0700

    better apple system

--- scribbs/apple.gd
+++ scribbs/apple.gd
@@ -1,8 +1,9 @@
 extends TileMapLayer
-var grid: Array[Array] = [[true]]
+var grid
 var lower_bounds := Vector2(1,1)
-var upper_bounds := Vector2(14,14)
+var upper_bounds := Vector2(13,13)
 var collector:Array[Vector2i]
+var possible_appels = [[[true]]]
 
 
 @onready var snek_daddy: Node2D = $"../Snek Daddy"
@@ -14,7 +15,9 @@ func _ready() -> void:
 func new_apple():
 	clear()
 	collector = []
-	var coords := Vector2(randi_range(lower_bounds.x,upper_bounds.x),randi_range(lower_bounds.y,upper_bounds.y))
+	grid = possible_appels.pick_random()
+	var coords := Vector2(randi_range(lower_bounds.x,15-len(grid[0])),randi_range(lower_bounds.y,15 - len(grid)))
+	
 	for x in range(0,len(grid)):
 		for y in range(0,len(grid[0])):
 			if grid[x][y]:
@@ -39,5 +42,22 @@ func check_appels(snek):
 	if snackable.all(func(x):return x):
 		new_apple()
 		snek_daddy.new_snek()
+		change_possibly_snakes(len(snek_daddy.snek_list))
 		
+func change_possibly_snakes(len):
+	if len == 10:
+		possible_appels.append([[true],[true]])
+		possible_appels.append([[true,true]])
+	elif len == 12:
+		possible_appels.append([[true,true],[true,false]])
+		possible_appels.append([[true,true,true]])
+		possible_appels.append([[false,true],[true,true]])
+	elif len == 13:
+		possible_appels.append([[true,true,false],[false,true,true]])
+		possible_appels.append([[false,true,true],[true,true,false]])
+	elif len == 14:
+		possible_appels.append([[false,true,false],[true,true,true]])
+		possible_appels.append([[false, true],[false, true],[true,true]])
+		possible_appels.append([[true, true],[true, false],[true,false]])
+	
 	
